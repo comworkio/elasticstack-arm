a6cccde8755cc7ae881b58531bb758e5cb0f1d6c
Idriss Neumann <idriss.neumann@comwork.io>
2021-01-06 13:40:57 +0100

    Add internal versions

--- ci/docker-deliver.sh
+++ ci/docker-deliver.sh
@@ -5,6 +5,7 @@ REPO_PATH="${BASE_DIR}/.."
 ARCH="${1}"
 IMAGE="${2}"
 VERSION="${3}"
+INTERNAL_VERSION="${4}"
 
 [[ $ARCH ]] || ARCH="x86"
 
@@ -22,9 +23,13 @@ echo "${DOCKER_ACCESS_TOKEN}" | docker login --username comworkio --password-std
 if [[ $ARCH == "x86" ]]; then
   docker-compose push "rollup_${ARCH}"
   tag_and_push "${VERSION}" "${IMAGE}"
+  [[ $INTERNAL_VERSION ]] && tag_and_push "${VERSION}-${INTERNAL_VERSION}" "${IMAGE}"
   tag_and_push "${VERSION}-${CI_COMMIT_SHORT_SHA}" "${IMAGE}"
+  [[ $INTERNAL_VERSION ]] && tag_and_push "${VERSION}-${INTERNAL_VERSION}-${CI_COMMIT_SHORT_SHA}" "${IMAGE}"
 fi
 
 tag_and_push "latest-${ARCH}" "${IMAGE}"
 tag_and_push "${VERSION}-${ARCH}" "${IMAGE}"
+[[ $INTERNAL_VERSION ]] && tag_and_push "${VERSION}-${INTERNAL_VERSION}-${ARCH}" "${IMAGE}"
 tag_and_push "${VERSION}-${ARCH}-${CI_COMMIT_SHORT_SHA}" "${IMAGE}"
+[[ $INTERNAL_VERSION ]] && tag_and_push "${VERSION}-${INTERNAL_VERSION}-${ARCH}-${CI_COMMIT_SHORT_SHA}" "${IMAGE}"
