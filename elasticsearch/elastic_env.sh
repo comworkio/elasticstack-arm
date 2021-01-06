185ba4cd58d5ac0d4a5c140bb5f138494e4ccb19
Idriss Neumann <idriss.neumann@comwork.io>
2021-01-06 19:54:51 +0100

    Fix: environment file

--- elasticsearch/elastic_env.sh
+++ elasticsearch/elastic_env.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 export ES_LOG_DIR="${ES_HOME}/logs"
 export ES_DATA_DIR="${ES_HOME}/data"
@@ -6,3 +6,7 @@ export ES_TMP_DIR="${ES_HOME}/tmp"
 export JAVA_HOME="/usr/lib/jvm/java-${JAVA_VERSION}-openjdk-armhf"
 export ES_JAVA_OPTS="-Djava.io.tmpdir=${ES_TMP_DIR} -Djna.tmpdir=${ES_TMP_DIR}"
 export PATH="${PATH}:/usr/share/elasticsearch/bin"
+
+mkdir -p "${ES_LOG_DIR}"
+mkdir -p "${ES_DATA_DIR}"
+mkdir -p "${ES_TMP_DIR}"
--- kibana/kibana_env.sh
+++ kibana/kibana_env.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 export PATH="${PATH}:/usr/share/elasticsearch/bin"
 export JAVA_HOME="/usr/lib/jvm/java-${JAVA_VERSION}-openjdk-armhf"
