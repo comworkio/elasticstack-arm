131facb7a458cf4d195df1e8395093b00b926ee6
Idriss Neumann <idriss.neumann@comwork.io>
2021-01-09 12:26:55 +0100

    Fix: server host

--- kibana/kibana_start.sh
+++ kibana/kibana_start.sh
@@ -2,6 +2,6 @@
 
 source /kibana_env.sh
 
-sed -i "s/ES_PROTO/${ES_PROTO}/g;s/ES_HOST/${ES_HOST}/g;s/ES_PORT/${ES_PORT}/g" "${KIBANA_HOME}/config/kibana.yml"
+sed -i "s/ES_PROTO/${ES_PROTO}/g;s/ES_HOST/${ES_HOST}/g;s/ES_PORT/${ES_PORT}/g;s/KIBANA_HOST/${SERVER_HOST}/g" "${KIBANA_HOME}/config/kibana.yml"
 
 /usr/share/kibana/kibana_start.sh
