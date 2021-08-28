93ac6bb63238c6e6f55418df9791c1b06eb71cb0
Idriss Neumann <idriss.neumann@comwork.io>
2021-08-28 10:03:14 +0100

    Use a different user to run elasticsearch (more secure)

--- elasticsearch/elastic_env.sh
+++ elasticsearch/elastic_env.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+[[ -f /elastic_root_env.sh ]] && source /elastic_root_env.sh
+
 export ES_LOG_DIR="${ES_HOME}/logs"
 export ES_DATA_DIR="${ES_HOME}/data"
 export ES_TMP_DIR="${ES_HOME}/tmp"
 
 export JAVA_HOME="/usr/lib/jvm/java-${JAVA_VERSION}-openjdk-armhf"
 export ES_JAVA_HOME="${JAVA_HOME}"
-export ES_JAVA_OPTS="-Djava.io.tmpdir=${ES_TMP_DIR} -Djna.tmpdir=${ES_TMP_DIR} -Des.insecure.allow.root=true"
+export ES_JAVA_OPTS="-Djava.io.tmpdir=${ES_TMP_DIR} -Djna.tmpdir=${ES_TMP_DIR}"
 export PATH="${PATH}:${ES_HOME}/bin"
 
 mkdir -p "${ES_LOG_DIR}"
