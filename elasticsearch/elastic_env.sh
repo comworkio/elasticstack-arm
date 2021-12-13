1d4b47714d2070206383cb3c8249122ab169df55
Idriss Neumann <idriss.neumann@comwork.io>
2021-12-13 19:41:43 +0100

    Fix: permissions

--- elasticsearch/elastic_env.sh
+++ elasticsearch/elastic_env.sh
@@ -6,7 +6,7 @@ export ES_TMP_DIR="${ES_HOME}/tmp"
 
 export JAVA_HOME="/usr/lib/jvm/java-${JAVA_VERSION}-openjdk-armhf"
 export ES_JAVA_HOME="${JAVA_HOME}"
-export ES_JAVA_OPTS="-Djava.io.tmpdir=${ES_TMP_DIR} -Djna.tmpdir=${ES_TMP_DIR} -Des.insecure.allow.root=true"
+export ES_JAVA_OPTS="-Djava.io.tmpdir=${ES_TMP_DIR} -Djna.tmpdir=${ES_TMP_DIR}"
 export PATH="${PATH}:${ES_HOME}/bin"
 
 mkdir -p "${ES_LOG_DIR}"
