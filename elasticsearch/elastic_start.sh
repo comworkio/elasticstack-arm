3c17808b8e1d856c77ccb6881c3f62bf292dede1
Idriss Neumann <idriss.neumann@comwork.io>
2021-12-13 20:59:26 +0100

    Fix: permissions error

--- elasticsearch/elastic_start.sh
+++ elasticsearch/elastic_start.sh
@@ -6,7 +6,4 @@ mkdir -p "${ES_LOG_DIR}"
 mkdir -p "${ES_DATA_DIR}"
 mkdir -p "${ES_TMP_DIR}"
 
-sudo chown -R elasticsearch:elasticsearch docker_data
-sudo chmod -R 755 $ES_HOME
-
 /usr/share/elasticsearch/es_start.sh
