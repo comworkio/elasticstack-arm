8fd441e4b68c0a885af9b129a114d4cbdaa318ad
Idriss Neumann <idriss.neumann@comwork.io>
2021-12-13 22:58:55 +0100

    Fix: missing variables

--- elasticsearch/elastic_env.sh
+++ elasticsearch/elastic_env.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-export ES_HOME="/usr/share/elasticsearch"
+export ES_HOME="VAL_ES_HOME"
+export JAVA_VERSION="VAL_JAVA_VERSION"
+
 export ES_LOG_DIR="${ES_HOME}/logs"
 export ES_DATA_DIR="${ES_HOME}/data"
 export ES_TMP_DIR="${ES_HOME}/tmp"
@@ -9,7 +11,3 @@ export JAVA_HOME="/usr/lib/jvm/java-${JAVA_VERSION}-openjdk-armhf"
 export ES_JAVA_HOME="${JAVA_HOME}"
 export ES_JAVA_OPTS="-Djava.io.tmpdir=${ES_TMP_DIR} -Djna.tmpdir=${ES_TMP_DIR}"
 export PATH="${PATH}:${ES_HOME}/bin"
-
-mkdir -p "${ES_LOG_DIR}"
-mkdir -p "${ES_DATA_DIR}"
-mkdir -p "${ES_TMP_DIR}"
